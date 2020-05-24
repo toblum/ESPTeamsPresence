c809ee467e6edcabf40735ed8e4506ef13174f79
Tobias Blum <tblum@outlook.com>
2020-05-24 23:57:22 +0200

    Bump version to 0.13.0

--- src/main.cpp
+++ src/main.cpp
@@ -55,7 +55,7 @@ const char* rootCACertificate = \
 #define DEFAULT_ERROR_RETRY_INTERVAL 30			// Default interval to try again after errors
 #define TOKEN_REFRESH_TIMEOUT 60	 			// Number of seconds until expiration before token gets refreshed
 #define CONTEXT_FILE "/context.json"			// Filename of the context file
-#define VERSION "0.12.0"						// Version of the software
+#define VERSION "0.13.0"						// Version of the software
 
 #define DBG_PRINT(x) Serial.print(x)
 #define DBG_PRINTLN(x) Serial.println(x)
